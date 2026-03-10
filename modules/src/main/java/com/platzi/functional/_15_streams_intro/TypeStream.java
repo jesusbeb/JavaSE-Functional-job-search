eb146178ae16a6ecbae5afd53d958df3982104bc
jesusbeb <jesusbelb@gmail.com>
2026-03-10 12:17:17 -0600

    Conversion de Strings a Listas de enteros en Java

--- modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
+++ modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
@@ -1,5 +1,7 @@
 package com.platzi.functional._15_streams_intro;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TypeStream {
@@ -8,13 +10,12 @@ public class TypeStream {
 
         // Un IntStream emite enteros, generamos uno que empiece de cero y lo haga de manera infinita, pero lo limitamos a 1000
         // Filtramos solo los pares e imprimimos
-        // parallel hace que el Stream se ejecute de manera concurrente, es decir si el procesador tiene 4 nucleos, se ejecutaran
-        // cuatro operaciones a la vez. Esto sirve para operaciones grandes y que no importe el orden de los resultados, ya que
-        // estos no se muestran continuos, sino de manera concurrente
+        // boxed() convierte un IntStream de enteros a Stream de Integer, recopilamos todos los datos con Collect y
+        // convertimos a un List
         IntStream infiniteStream = IntStream.iterate(0, x -> x + 1);
-        infiniteStream.limit(1000)
-                .parallel()
+        List<Integer> numbersList = infiniteStream.limit(1000)
                 .filter(x -> x%2 == 0)
-                .forEach(System.out::println);
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
